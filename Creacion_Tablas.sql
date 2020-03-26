f0bc14b7efa9b9e5c0590c425a4773c15aeae9cb
Ashly2015 <ranbr200015@gmail.com>
2020-03-26 11:27:16 -0600

    Creacion de tablas

---
+++ Creacion_Tablas.sql
@@ -0,0 +1,19 @@
+use bd_ins;
+create table estudiantes(
+Carne int(20) primary key auto_increment,
+NombreEstudiante varchar(60) not null,
+EdadEstudiante int not null,
+DireccionEstudiante varchar (60) not null,
+TelefonoEstudiante varchar(60) not null,
+CodigoMaestros int(20) not null,
+foreign key (CodigoMaestros) references
+maestros(CodigoMaestros)
+)engine=innodb;
+
+create table maestros(
+CodigoMaestros int(20) primary key auto_increment,
+NombreMaestro varchar(60) not null,
+DireccionMaestro varchar (60) not null,
+TelefonoMaestro varchar(60) not null,
+CorreoMaestros varchar(60) not null
+)engine=innodb;
